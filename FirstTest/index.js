faa9a6023c30d343dde6e72c119ed700a6c6a532
Denny <dennygod1220@gmail.com>
2017-12-28 22:23:44 +0800

    最多可以2個人 同時加入遊戲

--- FirstTest/index.js
+++ FirstTest/index.js
@@ -6,24 +6,33 @@ var PORT = 9999;
 var io = require('socket.io')(server);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
 server.listen(PORT);
 /* --------------------------SOCKET.IO---------------------------------------------------*/
-io.on('connection',function(socket){
+io.on('connection', function (socket) {
     console.log('a user is connect , this connect id is : ' + socket.id);
-//接收玩家的名字
-    socket.on('send name',function(data){
-        console.log('a user name is: '+data);
+    //接收玩家的名字
+    socket.on('send name', function (data) {
+        console.log('a user name is: ' + data);
         var name = data;
         //針對此玩家發送起始座標
-        io.sockets.connected[socket.id].emit('connect sucess',{name:name,x:getRandomInt(40,560),y:getRandomInt(40,560)});
+        io.sockets.connected[socket.id].emit('connect sucess', {
+            name: name,
+            x: getRandomInt(40, 560),
+            y: getRandomInt(40, 560)
+        });
     });
 
-    socket.on('disconnect',function(data){
-        console.log('a user is disconnect , id is : '+socket.id);
+
+    socket.on('user ball position', function (data) {
+        socket.broadcast.emit('other user position', data);
+    });
+
+    socket.on('disconnect', function (data) {
+        console.log('a user is disconnect , id is : ' + socket.id);
     });
 });
 
@@ -31,3 +40,14 @@ io.on('connection',function(socket){
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+
+
+
+
+
+
+
--- FirstTest/public/js/main.js
+++ FirstTest/public/js/main.js
@@ -13,9 +13,17 @@ $(function () {
 
 
 var game = new Phaser.Game(600, 600, Phaser.AUTO, 'mygame');
+//儲存自己的名字
 var name = null;
+//自己的球
 var ball;
+var BallGroup;
+//自己的球的初始位置
 var ballinitialX, ballinitialY;
+//其他玩家球的位置
+var otherBallX = 0;
+var otherBallY = 0;
+var OtherBall;
 var state = {
 
     preload: function () {
@@ -28,6 +36,7 @@ var state = {
             game.load.image("down", "img/down.png");
             game.load.image("right", "img/right.png");
             game.load.image("star", "img/star.png");
+            game.load.image('user', 'img/user.png');
             game.scale.pageAlignHorizontally = true;
             game.scale.pageAlignVertically = true;
         };
@@ -52,15 +61,71 @@ var state = {
         this.create = function () {
             game.stage.backgroundColor = '#FFFFE0';
             console.log("This Player Name : " + name);
+            /*創建 自己的 球 */
             ball = game.add.sprite(ballinitialX, ballinitialY, 'ball');
+            ball.anchor.set(0.5);
+            game.physics.enable(ball, Phaser.Physics.ARCADE);
+            ball.body.collideWorldBounds = true;
+            ball.body.velocity.x = 100;
+            ball.body.bounce.set(1);
+
+            /*創建 名字 */
+            text = game.add.text(ballinitialX, ballinitialY, name, '#fff');
+            text.fontSize = 10;
+            text.align = "center";
+            game.physics.enable(text, Phaser.Physics.ARCADE);
+
+            //建立其他玩家的球
+            OtherBall = game.add.sprite(0, 0, 'user');
+            game.physics.enable(OtherBall, Phaser.Physics.ARCADE);
+
         };
+
+        this.update = function () {
+            //讓名字和球一起移動
+            text.body.x = ball.body.x;
+            text.body.y = ball.body.y;
+            //將 自己的球 位置傳送給server
+            socket.emit('user ball position', {
+                name:name,
+                x: ball.body.x,
+                y: ball.body.y
+            });
+            //收聽廣播 收聽其他玩家球的位置 並將其他玩家的位置存到全域變數中
+            socket.on('other user position', function (data) {
+                console.log(data.name);
+                otherBallX = data.x;
+                otherBallY = data.y;
+            });
+            OtherBall.body.x = otherBallX;
+            OtherBall.body.y = otherBallY;
+        };
+
+
     },
 
-    play: function () {},
+    play: function () {
+
+    },
 
     over: function () {}
 };
 
+/*
+var userList = {};
+
+function addUser(id) {
+    var sprite = game.add.sprite(x, y, 'img');
+    sprite.anchor.x = 0.5;
+    userList[id] = sprite;
+}
+
+function init(playerList) {
+    playerList.forEach(function (player) {
+        addUser(player);
+    }, this);
+}
+*/
 
 Object.keys(state).map(function (key) {
     game.state.add(key, state[key]);
