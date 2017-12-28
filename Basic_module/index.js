68ed4b15143f4079bd4f09615ce75a57e8c36015
Denny <dennygod1220@gmail.com>
2017-12-28 15:33:37 +0800

    基本Socket.io連線範本

--- Basic_module/index.js
+++ Basic_module/index.js
@@ -6,16 +6,18 @@ var PORT = 9999;
 var io = require('socket.io')(server);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-server.listen(PORT);
+server.listen(PORT, function () {
+    console.log('Server is listen on PORT : ' + PORT);
+});
 /* --------------------------SOCKET.IO---------------------------------------------------*/
-io.on('connection',function(socket){
+io.on('connection', function (socket) {
     console.log('a user is connect , this connect id is : ' + socket.id);
-
-    socket.on('disconnect',function(data){
-        console.log('a user is disconnect , id is : '+socket.id);
+    //當使用者離開時的事件
+    socket.on('disconnect', function (data) {
+        console.log('a user is disconnect , id is : ' + socket.id);
     });
 });
